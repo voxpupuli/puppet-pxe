da298ae1829063f9115d2b474ca76c9bf41f434b
Zach <zach@puppetlabs.com>
2011-02-27 22:03:28 -0800

    add example node

---
+++ tests/node.pp
@@ -0,0 +1,16 @@
+class { 'pxe':
+  tftp_root => '/tftpboot',
+}
+
+class { 'pxe::bootstrap::settings':
+  location => '/var/www/bootstrap',
+}
+
+pxe::bootstrap {
+  "maverick.cfg":
+    os         => "ubuntu",
+    ver        => "maverick",
+    proxy      => "http://vanir.puppetlabs.lan:3142",
+    role       => "standard",
+}
+
