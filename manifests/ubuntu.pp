3ee743edfb3ea0e77afa5125cb851fa97f556c9c
Zach <zach@puppetlabs.com>
2011-02-27 22:35:06 -0800

    clean up

--- manifests/ubuntu.pp
+++ manifests/ubuntu.pp
@@ -1,24 +1,24 @@
 define pxe::ubuntu($arch,$ver) {
    
-    # http://mirrors.kernel.org/ubuntu/dists/lucid/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/
-    $baseurl = "http://mirrors.kernel.org/ubuntu/dists"
+	# http://mirrors.kernel.org/ubuntu/dists/lucid/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/
+	$baseurl = "http://mirrors.kernel.org/ubuntu/dists"
 
 	
 	$dirs = [
-		"${tftp_root}/images/ubuntu/${arch}/${ver}",
+    "${tftp_root}/images/ubuntu/${arch}/${ver}",
 	]
 
 	file { $dirs: ensure => directory, owner => root, group => root, mode => 755; }
 
 	exec {
-		"pull ubuntu pxe linux $arch $ver":
-			cwd => "${tftp_root}/images/ubuntu/${arch}/${ver}",
-			command => "/usr/bin/wget ${baseurl}/${ver}/main/installer-${arch}/current/images/netboot/ubuntu-installer/${arch}/linux",
-			creates => "${tftp_root}/images/ubuntu/${arch}/${ver}/linux";
-		"pull centos pxe initrd.img $arch $ver":
-			cwd => "${tftp_root}/images/ubuntu/${arch}/${ver}",
-			command => "/usr/bin/wget ${baseurl}/${ver}/main/installer-${arch}/current/images/netboot/ubuntu-installer/${arch}/initrd.gz",
-			creates => "${tftp_root}/images/ubuntu/${arch}/${ver}/initrd.gz";
+    "pull ubuntu pxe linux $arch $ver":
+      cwd => "${tftp_root}/images/ubuntu/${arch}/${ver}",
+      command => "/usr/bin/wget ${baseurl}/${ver}/main/installer-${arch}/current/images/netboot/ubuntu-installer/${arch}/linux",
+      creates => "${tftp_root}/images/ubuntu/${arch}/${ver}/linux";
+    "pull ubuntu pxe initrd.img $arch $ver":
+      cwd => "${tftp_root}/images/ubuntu/${arch}/${ver}",
+      command => "/usr/bin/wget ${baseurl}/${ver}/main/installer-${arch}/current/images/netboot/ubuntu-installer/${arch}/initrd.gz",
+      creates => "${tftp_root}/images/ubuntu/${arch}/${ver}/initrd.gz";
 	}
 
 	#file {
