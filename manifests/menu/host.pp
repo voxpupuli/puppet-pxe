69b3a1321345b9b280fc4d9ea8a50130f1b4e265
Zach <zach@puppetlabs.com>
2011-03-13 15:58:31 -0700

    add host support update readme to include an example

---
+++ manifests/menu/host.pp
@@ -0,0 +1,15 @@
+define pxe::menu::host ($kernel,$append) {
+
+  $tftp_root = $::pxe::tftp_root
+  $label = $title 
+
+  concat::fragment { "$title":
+    order => '00',
+    target => "$tftp_root/pxelinux.cfg/$title",
+    content => template("pxe/menuhost.erb"),
+  } 
+
+  concat { "$tftp_root/pxelinux.cfg/$title": }
+
+
+}
