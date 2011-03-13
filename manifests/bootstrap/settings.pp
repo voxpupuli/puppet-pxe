1a1d177e2809df19ba839d11c20b70201504603f
Zach <zach@puppetlabs.com>
2011-03-13 15:05:43 -0700

    add additional bootstrap::settings arguments: rootpw

--- manifests/bootstrap/settings.pp
+++ manifests/bootstrap/settings.pp
@@ -1,6 +1,7 @@
 class pxe::bootstrap::settings (
-    $location, # where to store the bootsrtap files
-    $ntpserver="pool.ntp.org"
+    $location,   # where to store the bootsrtap files
+    $ntpserver = "pool.ntp.org",
+    $rootpw    = "toor"
   ) {
 
 }
