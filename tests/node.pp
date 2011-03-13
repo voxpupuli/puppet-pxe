94177ae673e35e462f40885c2ecb4ff687048821
Zach <zach@puppetlabs.com>
2011-03-13 15:08:00 -0700

    update tests

--- tests/node.pp
+++ tests/node.pp
@@ -2,8 +2,16 @@ class { 'pxe':
   tftp_root => '/tftpboot',
 }
 
+
+class { 'pxe::bootstrap::settings':
+  location  => '/var/www/bootstrap',
+  ntpserver => "wall.znet",
+  rootpw    => 'changeme'
+}
+
 class { 'pxe::bootstrap::settings':
   location => '/var/www/bootstrap',
+  ntpserver => 
 }
 
 pxe::bootstrap {
@@ -15,9 +23,15 @@ pxe::bootstrap {
     arch       => "amd64",
 }
 
+
+# TO ADD:
 pxe::host {
   "server1.example.net":
     hwaddr => "01-aa-bb-cc-dd-ee-ff",
     bootstrap => "pxe/preseed-server1.example.net.erb",
+}
+
+pxe::bootstrap::preseed::settings {
+  upgrade => true,
 }
 
