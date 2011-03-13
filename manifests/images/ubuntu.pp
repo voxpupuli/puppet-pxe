2980569531b6b7d172aac63c048410a7284c6005
Zach <zach@puppetlabs.com>
2011-03-12 23:50:26 -0800

    remove old cruft

--- manifests/images/ubuntu.pp
+++ manifests/images/ubuntu.pp
@@ -1,22 +1,16 @@
 define pxe::images::ubuntu($arch,$ver) {
 
-#  pxe::menu::ubuntu::entry { 
-#    "Ubuntu $ver $arch Preseed Install":
-#       kernel => "images/ubuntu/$ver/$arch/linux",
-#       initrd => "images/ubuntu/$ver/$arch/initrd.gz",
-#  }
-
   # http://mirrors.kernel.org/ubuntu/dists/lucid/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/
+  $path    = "$ver/main/installer-$arch/current/images/netboot/ubuntu-installer/$arch"
 	$baseurl = "http://mirrors.kernel.org/ubuntu/dists"
-  $path = "$ver/main/installer-$arch/current/images/netboot/ubuntu-installer/$arch"
 
 	exec {
 		"wget ubuntu pxe linux $arch $ver":
-			cwd => "$tftp_root/images/ubuntu/$ver/$arch",
+			cwd     => "$tftp_root/images/ubuntu/$ver/$arch",
 			command => "/usr/bin/wget $baseurl/$path/linux",
 			creates => "$tftp_root/images/ubuntu/$ver/$arch/linux";
 		"wget ubuntu pxe initrd.img $arch $ver":
-			cwd => "$tftp_root/images/ubuntu/$ver/$arch",
+			cwd     => "$tftp_root/images/ubuntu/$ver/$arch",
 			command => "/usr/bin/wget $baseurl/$path/initrd.gz",
 			creates => "$tftp_root/images/ubuntu/$ver/$arch/initrd.gz";
 	}
