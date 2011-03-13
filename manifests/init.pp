e7d8cb800caebc88fef370e710b40b690f79bdc4
Zach <zach@puppetlabs.com>
2011-03-12 23:50:06 -0800

    clean up align hashrockets remove old comments

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,101 +2,81 @@ class pxe ($tftp_root='/tftpboot'){
   include apache
   include pxe::menu
 
-	package {
-		"xinetd":				ensure => installed;
-		"syslinux":			ensure => installed;
-		"tftp-server":	
-      name => $operatingsystem ? {
-        Ubuntu => "tftpd-hpa",
+  package {
+    "xinetd":   ensure => installed;
+    "syslinux": ensure => installed;
+  }
+
+  package {
+    "tftp-server":	
+      name   => $operatingsystem ? {
+        Ubuntu  => "tftpd-hpa",
         default => "tftp-server",
       },
       ensure => installed;
 	}
 
-	file {
-		"/etc/xinetd.d/tftp":
-			owner		=> root,
-			group		=> root,
-			mode		=> 644,
-			notify	=> Service["xinetd"],
-			content	=> template("pxe/tftp_xinetd.erb");
-		"${tftp_root}": 
-      ensure => directory, 
-      owner => root, 
-      group => root, 
-      mode => 755;
-		"${tftp_root}/pxelinux.0": 
-      ensure => directory, 
-      owner => root, 
-      group => root, 
-      mode => 755,
-			source => "/usr/lib/syslinux/pxelinux.0",
-      require => Package["syslinux"];
-		"${tftp_root}/menu.c32": 
-      ensure => directory, 
-      owner => root, 
-      group => root, 
-      mode => 755,
-			source => "/usr/lib/syslinux/menu.c32",
-      require => Package["syslinux"];
-		"${tftp_root}/memdisk": 
-      ensure => directory, 
-      owner => root, 
-      group => root, 
-      mode => 755,
-			source => "/usr/lib/syslinux/memdisk",
-      require => Package["syslinux"];
-		"${tftp_root}/mboot.c32": 
-      ensure => directory, 
-      owner => root, 
-      group => root, 
-      mode => 755,
-			source => "/usr/lib/syslinux/mboot.c32",
-      require => Package["syslinux"];
-		"${tftp_root}/chain.c32": 
-      ensure => directory, 
-      owner => root, 
-      group => root, 
-      mode => 755,
-			source => "/usr/lib/syslinux/chain.c32",
-      require => Package["syslinux"];
-		"${tftp_root}/pxelinux.cfg": 
-      ensure => directory, 
-      owner => root, 
-      group => root, 
-      mode => 755;
-#		"${tftp_root}/images": 
-#      ensure => directory, 
-#      owner => root, 
-#      group => root, 
-#      mode => 755;
-#		#"${ks_root}": ensure => directory, owner => root, group => root, mode => 755;
-	}
+  file {
+    "/etc/xinetd.d/tftp":
+      owner     => root,
+      group     => root,
+      mode      => 644,
+      notify    => Service["xinetd"],
+      content   => template("pxe/tftp_xinetd.erb");
+    "${tftp_root}": 
+      ensure    => directory, 
+      owner     => root, 
+      group     => root, 
+      mode      => 755;
+    "${tftp_root}/pxelinux.0": 
+      ensure    => directory, 
+      owner     => root, 
+      group     => root, 
+      mode      => 755,
+      source    => "/usr/lib/syslinux/pxelinux.0",
+      require   => Package["syslinux"];
+    "${tftp_root}/menu.c32": 
+      ensure    => directory, 
+      owner     => root, 
+      group     => root, 
+      mode      => 755,
+      source    => "/usr/lib/syslinux/menu.c32",
+      require   => Package["syslinux"];
+    "${tftp_root}/memdisk": 
+      ensure    => directory, 
+      owner     => root, 
+      group     => root, 
+      mode      => 755,
+      source    => "/usr/lib/syslinux/memdisk",
+      require   => Package["syslinux"];
+    "${tftp_root}/mboot.c32": 
+      ensure    => directory, 
+      owner     => root, 
+      group     => root, 
+      mode      => 755,
+      source    => "/usr/lib/syslinux/mboot.c32",
+      require   => Package["syslinux"];
+    "${tftp_root}/chain.c32": 
+      ensure    => directory, 
+      owner     => root, 
+      group     => root, 
+      mode      => 755,
+      source    => "/usr/lib/syslinux/chain.c32",
+      require   => Package["syslinux"];
+    "${tftp_root}/pxelinux.cfg": 
+      ensure    => directory, 
+      owner     => root, 
+      group     => root, 
+      mode      => 755;
+  }
 
-	service {
-		"xinetd":
-			ensure => running,
-			enable => true;
-	}
+  service {
+    "xinetd":
+      ensure => running,
+      enable => true;
+  }
 
   Pxe::Images <| |>
 
-#file { $dirs: ensure => directory, owner => root, group => root, mode => 755, checksum => none; }
-	
-#	pxe::centos { 
-##		"centos_i386_4.8":
-##			arch => "i386",
-##			ver => "4.8";
-##		"centos_x86_64_4.8":
-##			arch => "x86_64",
-##			ver => "4.8";
-#		"centos_i386_5.5":
-#			arch => "i386",
-#			ver => "5.5";
-#		"centos_x86_64_5.5":
-#			arch => "x86_64",
-#			ver => "5.5";
-#	}
-#    
+}
 
-}	
