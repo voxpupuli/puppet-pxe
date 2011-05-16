3e59ceea5f3d40717babcc9a7737ed30c9f0cf83
Zach Leslie <zach@puppetlabs.com>
2011-05-16 14:44:43 -0700

    change install location remove tftp relate resources into Class[tftp]

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,82 +1,56 @@
-class pxe ($tftp_root='/tftpboot'){
-  include apache
-  include pxe::menu::default
+class pxe ($tftp_root='/srv/tftp'){
+  class { apache: }
+  class { tftp:               tftp_root => $tftp_root; }
+  class { "pxe::menu::default": tftp_root => $tftp_root; }
 
   package {
-    "xinetd":   ensure => installed;
     "syslinux": ensure => installed;
   }
 
-  package {
-    "tftp-server":	
-      name   => $operatingsystem ? {
-        Ubuntu  => "tftpd-hpa",
-        default => "tftp-server",
-      },
-      ensure => installed;
-	}
+  Pxe::Images <| |>
 
   file {
-    "/etc/xinetd.d/tftp":
+    "${tftp_root}/pxelinux.0":
+      ensure    => directory,
       owner     => root,
       group     => root,
-      mode      => 644,
-      notify    => Service["xinetd"],
-      content   => template("pxe/tftp_xinetd.erb");
-    "${tftp_root}": 
-      ensure    => directory, 
-      owner     => root, 
-      group     => root, 
-      mode      => 755;
-    "${tftp_root}/pxelinux.0": 
-      ensure    => directory, 
-      owner     => root, 
-      group     => root, 
       mode      => 755,
       source    => "/usr/lib/syslinux/pxelinux.0",
       require   => Package["syslinux"];
-    "${tftp_root}/menu.c32": 
-      ensure    => directory, 
-      owner     => root, 
-      group     => root, 
+    "${tftp_root}/menu.c32":
+      ensure    => directory,
+      owner     => root,
+      group     => root,
       mode      => 755,
       source    => "/usr/lib/syslinux/menu.c32",
       require   => Package["syslinux"];
-    "${tftp_root}/memdisk": 
-      ensure    => directory, 
-      owner     => root, 
-      group     => root, 
+    "${tftp_root}/memdisk":
+      ensure    => directory,
+      owner     => root,
+      group     => root,
       mode      => 755,
       source    => "/usr/lib/syslinux/memdisk",
       require   => Package["syslinux"];
-    "${tftp_root}/mboot.c32": 
-      ensure    => directory, 
-      owner     => root, 
-      group     => root, 
+    "${tftp_root}/mboot.c32":
+      ensure    => directory,
+      owner     => root,
+      group     => root,
       mode      => 755,
       source    => "/usr/lib/syslinux/mboot.c32",
       require   => Package["syslinux"];
-    "${tftp_root}/chain.c32": 
-      ensure    => directory, 
-      owner     => root, 
-      group     => root, 
+    "${tftp_root}/chain.c32":
+      ensure    => directory,
+      owner     => root,
+      group     => root,
       mode      => 755,
       source    => "/usr/lib/syslinux/chain.c32",
       require   => Package["syslinux"];
-    "${tftp_root}/pxelinux.cfg": 
-      ensure    => directory, 
-      owner     => root, 
-      group     => root, 
+    "${tftp_root}/pxelinux.cfg":
+      ensure    => directory,
+      owner     => root,
+      group     => root,
       mode      => 755;
   }
-
-  service {
-    "xinetd":
-      ensure => running,
-      enable => true;
-  }
-
-  Pxe::Images <| |>
 
 }
 
