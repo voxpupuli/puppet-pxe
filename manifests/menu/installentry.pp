4385675e999b245eb3ce4b7d37c4a5ec31e940a9
Zach Leslie <zach@puppetlabs.com>
2011-09-13 16:23:55 -0700

    add menu::installentry and supporting tempalte

---
+++ manifests/menu/installentry.pp
@@ -0,0 +1,39 @@
+# Class: pxe::menu::installentry
+#
+# This will create an entry, but also accepts things like arch and version
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+define pxe::menu::installentry (
+  $order    ='10',
+  $kernel,
+  $append,
+  $file,
+  $arch,
+  $os,
+  $ver,
+  $template = "pxe/menuinstallentry.erb") {
+
+  include concat::setup
+
+  $tftp_root = $::pxe::tftp_root
+  $fullpath  = "$tftp_root/pxelinux.cfg"
+
+  $menu_file     = inline_template($file)
+  $append_string = inline_template($append)
+  $kernel_string = inline_template($kernel)
+
+  concat::fragment { "install-menu-${os}-${ver}-${arch}":
+    order   => $order,
+    target  => "$fullpath/$menu_file",
+    content => template("pxe/menuinstallentry.erb"),
+  }
+
+}
+
