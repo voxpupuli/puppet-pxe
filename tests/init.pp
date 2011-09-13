11f6d5e129c5ae416f82aeab89016ddbe5832212
Zach Leslie <zach@puppetlabs.com>
2011-09-13 16:24:25 -0700

    fiddle with some tests

--- tests/init.pp
+++ tests/init.pp
@@ -6,6 +6,12 @@ $ubuntu = {
   "os"   => "ubuntu"
 }
 
+$ubuntu_menu = {
+  file    => "os_<%= os %>",
+  kernel  => "images/<%= os %>/<%= arch %>/<%= ver %>/linux",
+  append  => "vga=791 initrd=images/<%= os %>/<%= arch %>/<%= ver %>/initrd.gz auto locale=en_US console-setup/layoutcode=us netcfg/get_hostname=debian url=http://urd.puppetlabs.lan/seed/ubuntu_base.cfg text",
+}
+
 $debian = {
   "arch" => ["amd64","i386"],
   "ver"  => ["lenny","squeeze","wheezy"],
@@ -13,18 +19,21 @@ $debian = {
 }
 
 $debian_menu = {
-  file    => "os_<%= os %>",
-  kernel  => "images/<%= os %>/<%= arch %>/<%= ver %>/linux",
-  append  => "vga=791 initrd=images/<%= os %>/<%= arch %>/<%= ver %>/initrd.gz auto locale=en_US console-setup/layoutcode=us netcfg/get_hostname=debian url=http://urd.puppetlabs.lan/seed/debian_base.cfg text",
+  "file"    => "os_<%= os %>",
+  "kernel"  => "images/<%= os %>/<%= arch %>/<%= ver %>/linux",
+  "append"  => "vga=791 initrd=images/<%= os %>/<%= arch %>/<%= ver %>/initrd.gz auto locale=en_US console-setup/layoutcode=us netcfg/get_hostname=<%= os %> url=http://urd.puppetlabs.lan/seed/debian_base.cfg text",
 }
-
-resource_permute('pxe::menu::installentry', $debian, $debian_menu)
-
 
 $centos = {
   "arch" => ["x86_64","i386"],
   "ver"  => [4,5,6],
   "os"   => "centos"
+}
+
+$centos_menu = {
+  "file"    => "os_<%= os %>",
+  "kernel"  => "images/<%= os %>/<%= arch %>/<%= ver %>/vmlinuz",
+  "append"  => "initrd=images/<%= os %>/<%= arch %>/<%= ver %>/initrd.img ks=http://urd.puppetlabs.lan/ks/centos_kickstart.cfg text",
 }
 
 $redhat = {
@@ -35,6 +44,12 @@ $redhat = {
 
 $redhat_common = {
   "baseurl" => "http://yo.puppetlabs.lan/rhel<%= ver %>server-<%= arch %>/disc1/images/pxeboot"
+}
+
+$redhat_menu = {
+  "file"    => "os_<%= os %>",
+  "kernel"  => "images/<%= os %>/<%= arch %>/<%= ver %>/vmlinuz",
+  "append"  => "initrd=images/<%= os %>/<%= arch %>/<%= ver %>/initrd.img ks=http://urd.puppetlabs.lan/ks/<% os %><%= ver %>-<%= arch %>.cfg text",
 }
 
 $scientific = {
@@ -54,13 +69,22 @@ $fedora = {
 }
 
 $fedora_common = {
-  "baseurl" => "http://mirrors.kernel.org/fedora/releases/<%= ver %>/Fedora/<%= arch %>/os/images/pxeboot/"
+  "baseurl" => "http://mirrors.kernel.org/fedora/releases/<%= ver %>/Fedora/<%= arch %>/os/images/pxeboot/",
+  "file"    => "os_<%= os %>",
+  "kernel"  => "images/<%= os %>/<%= arch %>/<%= ver %>/vmlinuz",
+  "append"  => "initrd=images/<%= os %>/<%= arch %>/<%= ver %>/initrd.img ks=http://urd.puppetlabs.lan/ks/<% os %><%= ver %>-<%= arch %>.cfg text",
 }
 
 resource_permute('pxe::images', $ubuntu)
 resource_permute('pxe::images', $debian)
 resource_permute('pxe::images', $centos)
-resource_permute('pxe::images', $redhat, $redhat_common)
+resource_permute('pxe::images', $redhat,     $redhat_common)
 resource_permute('pxe::images', $scientific, $scientific_common)
-resource_permute('pxe::images', $fedora, $fedora_common)
+resource_permute('pxe::images', $fedora,     $fedora_common)
+
+resource_permute('pxe::menu::installentry', $debian, $debian_menu)
+resource_permute('pxe::menu::installentry', $ubuntu, $ubuntu_menu)
+
+resource_permute('pxe::bootstrap', $ubuntu, $ubuntu_menu)
+
 
