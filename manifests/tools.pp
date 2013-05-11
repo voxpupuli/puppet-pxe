4fccb5ad2ffe3699a566cc0786ade4a518385748
William Van Hevelingen <blkperl@cat.pdx.edu>
2013-05-11 03:44:30 +0000

    Remove duplicate tool dir file definition

--- manifests/tools.pp
+++ manifests/tools.pp
@@ -3,16 +3,12 @@ class pxe::tools {
   $tftp_root = $::pxe::tftp_root
 
   # Create the directory to store all the tool images
-  file { "${tftp_root}/tools}":
+  file { "${tftp_root}/tools":
     ensure => directory,
   }
 
   # Create the tool menu
   pxe::menu { "Tools": file => "menu_tools", }
-
-  file { "${tftp_root}/tools":
-    ensure => directory,
-  }
 
   # Bring in some tools
   include pxe::tools::memtest
