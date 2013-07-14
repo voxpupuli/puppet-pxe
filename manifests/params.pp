ebf62ae603ca1756cd6def8f29d502f1b83895cd
Zach Leslie <zach@puppetlabs.com>
2013-07-14 12:53:10 -0700

    update version again

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,5 @@
 class pxe::params {
 
-  $syslinux_archive = "https://www.kernel.org/pub/linux/utils/boot/syslinux/4.xx/syslinux-4.06.tar.gz"
-  $syslinux_dir     = "/usr/local/src/syslinux-4.04"
-
+  $syslinux_archive = "http://www.kernel.org/pub/linux/utils/boot/syslinux/4.xx/syslinux-4.06.tar.gz"
+  $syslinux_dir     = "/usr/local/src/syslinux-4.06"
 }
