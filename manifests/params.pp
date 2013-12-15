3c5967fe3b252281ce50c0690accbe81be13ad83
Zach Leslie <zach@puppetlabs.com>
2013-12-14 22:47:28 -0800

    use https

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,5 @@
 class pxe::params {
 
-  $syslinux_archive = "http://www.kernel.org/pub/linux/utils/boot/syslinux/4.xx/syslinux-4.06.tar.gz"
+  $syslinux_archive = "https://www.kernel.org/pub/linux/utils/boot/syslinux/4.xx/syslinux-4.06.tar.xz"
   $syslinux_dir     = "/usr/local/src/syslinux-4.06"
 }
