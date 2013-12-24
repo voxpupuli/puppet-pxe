3b3c50529730446ee81df16c821df5e9706107c3
Zach Leslie <zach@puppetlabs.com>
2013-12-24 10:54:38 -0800

    (maint) archives are now hosted over https

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class pxe::params {
 
-  $syslinux_archive = "http://www.kernel.org/pub/linux/utils/boot/syslinux/4.xx/syslinux-4.04.tar.gz"
+  $syslinux_archive = "https://www.kernel.org/pub/linux/utils/boot/syslinux/4.xx/syslinux-4.04.tar.gz"
   $syslinux_dir     = "/usr/local/src/syslinux-4.04"
 
 }
