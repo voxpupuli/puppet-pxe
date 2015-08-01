6707251bea2a174eb32a0e9fd59f0be5cf62b3ec
Peter Foley <peter@ifoley.id.au>
2015-08-02 00:59:41 +1000

    Update to use ver 6.03 of syslinux

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,6 +4,6 @@
 #
 class pxe::params {
 
-  $syslinux_archive = 'https://www.kernel.org/pub/linux/utils/boot/syslinux/4.xx/syslinux-4.04.tar.gz'
-  $syslinux_dir     = '/usr/local/src/syslinux-4.04'
+  $syslinux_archive = 'https://www.kernel.org/pub/linux/utils/boot/syslinux/6.xx/syslinux-6.03.tar.gz'
+  $syslinux_dir     = '/usr/local/src/syslinux-6.03'
 }
