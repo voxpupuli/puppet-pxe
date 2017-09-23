b8c90316d90fe652e3f7f0f601f734fb0c164827
Zach Leslie <xaque208@gmail.com>
2017-09-23 12:10:10 -0700

    Update from xaque208 modulesync_config

--- spec/defines/pxe_installer_spec.rb
+++ spec/defines/pxe_installer_spec.rb
@@ -3,28 +3,27 @@ require 'spec_helper'
 describe 'pxe::installer' do
   on_supported_os.each do |os, facts|
     context "on #{os}" do
-      let(:facts) { facts.merge({:concat_basedir => '/dne'}) }
+      let(:facts) { facts.merge(concat_basedir: '/dne') }
 
       context 'simple' do
         let(:title) { 'debian_potato_amd64' }
-        let(:params) { {
-          :arch => 'amd64',
-          :ver => 'potato',
-          :os => 'debian',
-          :file => 'os_debian',
-          :kernel => 'images/debian/potato/amd64/linux',
-          :append => 'initrd=images/debian/potato/amd64/initrd.gz text'
-        } }
+        let(:params) do
+          {
+            arch: 'amd64',
+            ver: 'potato',
+            os: 'debian',
+            file: 'os_debian',
+            kernel: 'images/debian/potato/amd64/linux',
+            append: 'initrd=images/debian/potato/amd64/initrd.gz text'
+          }
+        end
 
         it do
           is_expected.to contain_pxe__installer('debian_potato_amd64')
-          is_expected.to contain_pxe__menu__entry('Installer: debian potato amd64').with({
-            'kernel' => 'images/debian/potato/amd64/linux',
-            'append' => 'initrd=images/debian/potato/amd64/initrd.gz text'
-          })
+          is_expected.to contain_pxe__menu__entry('Installer: debian potato amd64').with('kernel' => 'images/debian/potato/amd64/linux',
+                                                                                         'append' => 'initrd=images/debian/potato/amd64/initrd.gz text')
         end
       end
-
     end
   end
 end
