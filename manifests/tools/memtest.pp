3e48fd7f28cc90840955543538233f8fd0672880
Mike Renfro <mike+github@renf.ro>
2020-10-18 13:02:50 -0500

    Fixing travis whitespace

--- manifests/tools/memtest.pp
+++ manifests/tools/memtest.pp
@@ -6,7 +6,6 @@ class pxe::tools::memtest (
   $url = 'http://www.memtest.org/download/4.20/memtest86+-4.20.bin.gz',
   $tftp_root = $pxe::tftp_root
 ) {
-
   archive { "${tftp_root}/tools/memtest86+-4.20.bin.gz":
     ensure       => present,
     source       => $url,
