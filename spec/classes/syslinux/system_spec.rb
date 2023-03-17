e52019fe2679e6c9971adce9e7e59b937ea7e049
Mike Renfro <renfro@tntech.edu>
2023-03-17 14:53:59 -0500

    Update test spec

--- spec/classes/syslinux/system_spec.rb
+++ spec/classes/syslinux/system_spec.rb
@@ -11,8 +11,12 @@ describe 'pxe::syslinux::system' do
       }
     end
 
+    it { is_expected.to contain_class('pxe::syslinux::direct') }
     it { is_expected.to contain_package('syslinux') }
     it { is_expected.to contain_file('/srv/tftp/pxelinux.0') }
+    it { is_expected.to contain_file('/srv/tftp/syslinux/ldlinux.c32') }
+    it { is_expected.to contain_file('/srv/tftp/syslinux/libcom32.c32') }
+    it { is_expected.to contain_file('/srv/tftp/syslinux/libutil.c32') }
     it { is_expected.to contain_File('/srv/tftp/syslinux/memdisk') }
     it { is_expected.to contain_File('/srv/tftp/syslinux/menu.c32') }
     it { is_expected.to contain_file('/srv/tftp/syslinux/reboot.c32') }
